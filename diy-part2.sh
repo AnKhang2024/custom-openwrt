01134ae43699a27aced0b8f42cafbf6a17b71c26
Thanh Phat <85094758+InBomBi@users.noreply.github.com>
2024-09-20 08:38:36 +0700

    Update diy-part2.sh
    change ruby HASH

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/3.3.4/3.2.5/g' feeds/packages/lang/ruby/Makefile
+sed -i 's/3.3.4/3.2.2/g' feeds/packages/lang/ruby/Makefile
+sed -i 's/fe6a30f97d54e029768f2ddf4923699c416cdbc3a6e96db3e2d5716c7db96a34/96c57558871a6748de5bc9f274e93f4b5aad06cd8f37befa0e8d94e7b8a423bc/g' feeds/packages/lang/ruby/Makefile 
