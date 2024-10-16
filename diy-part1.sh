96ebd1566771f843d4d8511f671f2c277276d7fe
Thanh Phat <85094758+InBomBi@users.noreply.github.com>
2024-10-16 09:38:08 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git batman https://git.open-mesh.org/openwrt-feed-devel.git' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git passwall_2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+#echo 'src-git batman https://git.open-mesh.org/openwrt-feed-devel.git' >> feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall_2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
+echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main >> feeds.conf.default
