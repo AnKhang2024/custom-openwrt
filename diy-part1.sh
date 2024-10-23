d46dfdbb97de434afbb235cdcfdc719b2ba62ff7
Thanh Phat <85094758+InBomBi@users.noreply.github.com>
2024-10-23 08:22:50 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 echo 'src-git passwall_2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
-echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main >> feeds.conf.default
+echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
